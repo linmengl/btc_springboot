18bb4e71be6f3c12a04d303496994dd151860523
menglin319 <33089669+menglin319@users.noreply.github.com>
2018-07-17 16:31:28 +0800

    springboot

---
+++ src/main/java/com/blockchain/btc_springboot/BtcSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.blockchain.btc_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtcSpringbootApplication.class, args);
+	}
+}
